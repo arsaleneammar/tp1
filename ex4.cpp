5a4561325d82b333ab72254e6866729b2b4cdc78
arsaleneammar <arsalene.ammar@isimg.tn>
2024-12-30 15:46:43 +0100

    Add files via upload

--- ex4.cpp
+++ ex4.cpp
@@ -2,28 +2,102 @@
 #include <cmath>
 using namespace std;
 
-double f(double x, bool &OK) {
-    if (x <= 1 || x >= 2) {
-        OK = false;
-        return 0;
-    } else {
-        OK = true;
-        return sqrt((x - 1) * (2 - x));
+class Complexe {
+private:
+    double Re, Img;
+
+public:
+    Complexe(double re = 0.0, double img = 0.0) : Re(re), Img(img) {}
+
+    void afficher() const {
+        cout << Re << " + " << Img << "i" << endl;
     }
-}
+
+    double module() const {
+        return sqrt(Re * Re + Img * Img);
+    }
+
+    Complexe conjugue() const {
+        return Complexe(Re, -Img);
+    }
+
+    Complexe operator+(const Complexe& c) const {
+        return Complexe(Re + c.Re, Img + c.Img);
+    }
+
+    Complexe operator+(double d) const {
+        return Complexe(Re + d, Img);
+    }
+
+    friend Complexe operator+(double d, const Complexe& c) {
+        return Complexe(c.Re + d, c.Img);
+    }
+
+    Complexe operator-(const Complexe& c) const {
+        return Complexe(Re - c.Re, Img - c.Img);
+    }
+
+    Complexe operator-(double d) const {
+        return Complexe(Re - d, Img);
+    }
+
+    friend Complexe operator-(double d, const Complexe& c) {
+        return Complexe(d - c.Re, -c.Img);
+    }
+
+    Complexe operator*(const Complexe& c) const {
+        return Complexe(Re * c.Re - Img * c.Img, Re * c.Img + Img * c.Re);
+    }
+
+    Complexe operator*(double d) const {
+        return Complexe(Re * d, Img * d);
+    }
+
+    friend Complexe operator*(double d, const Complexe& c) {
+        return Complexe(c.Re * d, c.Img * d);
+    }
+};
 
 int main() {
-    double x;
-    bool OK;
-    cout << "Entrez x: ";
-    cin >> x;
+    Complexe c1(3.0, 4.0), c2(1.0, 2.0);
+    double val = 2.0;
 
-    double result = f(x, OK);
-    if (OK) {
-        cout << "La racine carrée est: " << result << endl;
-    } else {
-        cout << "La fonction n'est pas définie pour x = " << x << endl;
-    }
+    cout << "Complexe c1 : ";
+    c1.afficher();
+
+    cout << "Complexe c2 : ";
+    c2.afficher();
+
+    cout << "Somme c1 + c2 : ";
+    (c1 + c2).afficher();
+
+    cout << "Somme c1 + " << val << " : ";
+    (c1 + val).afficher();
+
+    cout << "Somme " << val << " + c2 : ";
+    (val + c2).afficher();
+
+    cout << "Différence c1 - c2 : ";
+    (c1 - c2).afficher();
+
+    cout << "Différence c1 - " << val << " : ";
+    (c1 - val).afficher();
+
+    cout << "Différence " << val << " - c2 : ";
+    (val - c2).afficher();
+
+    cout << "Produit c1 * c2 : ";
+    (c1 * c2).afficher();
+
+    cout << "Produit c1 * " << val << " : ";
+    (c1 * val).afficher();
+
+    cout << "Produit " << val << " * c2 : ";
+    (val * c2).afficher();
+
+    cout << "Module de c1 : " << c1.module() << endl;
+    cout << "Conjugué de c1 : ";
+    c1.conjugue().afficher();
 
     return 0;
 }
